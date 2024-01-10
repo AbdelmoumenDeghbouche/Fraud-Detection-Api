f7e06eb4e94517a3a0dedbefe728ece74145f560
Dtctv129 <ah.belagha@esi-sba.dz>
2024-01-10 11:56:37 +0100

    loc

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
     origins = [
     "http://localhost:3000",  
 ]
